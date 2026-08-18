2d856f02f3edee8c8d0d9403a1cd0d7af2853979
ushakrishnegowda06 <ushakrishnegowda06@gmail.com>
2026-08-18 09:14:19 +0530

    Add files via upload

---
+++ DivideTwoNumber.java
@@ -0,0 +1,14 @@
+package mm;
+
+public class DivideTwoNumber {
+
+	public static void main(String[] args) {
+		int a = 10;
+        int b = 2;
+        int c = a / b;
+
+        System.out.println("Division = " + c);
+
+	}
+
+}
---
+++ fibo.java
@@ -0,0 +1,20 @@
+package fib;
+
+public class fibo {
+	    public static void main(String[] args) {
+	        int n = 10; 
+
+	        int first = 0, second = 1;
+
+	        System.out.println("Fibonacci Series:");
+
+	        for (int i = 1; i <= n; i++) {
+	            System.out.print(first + " ");
+
+	            int next = first + second;
+	            first = second;
+	            second = next;
+	        }
+	    }
+	}
+
