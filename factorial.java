a18ee4f0d382d6c23f3c9a034abaccfe55dccd96
ushakrishnegowda06 <ushakrishnegowda06@gmail.com>
2026-08-18 09:07:57 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,18 @@
+package fff;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		        int n = 5;
+		        int factorial = 1;
+
+		        for (int i = 1; i <= n; i++) {
+		            factorial = factorial * i;
+		        }
+
+		        System.out.println("Factorial of " + n + " is " + factorial);
+		    		}
+
+	}
+
+
