5ec642a882ee76d17ad7c42bf491a5f03fb4ee67
ushakrishnegowda06 <ushakrishnegowda06@gmail.com>
2026-08-18 09:20:41 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,16 @@
+package aaaa;
+
+public class circle {
+
+	public static void main(String[] args) {
+		        double radius = 5;
+		        double area = 3.14 * radius * radius;
+
+		        System.out.println("Area of Circle = " + area);
+		    }
+		
+		    }
+		
+
+	
+
