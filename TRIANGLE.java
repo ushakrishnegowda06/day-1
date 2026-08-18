0a1bb516f35eaa308ee3a42bed5014b6ffd1dab1
ushakrishnegowda06 <ushakrishnegowda06@gmail.com>
2026-08-18 10:43:17 +0530

    Add files via upload

---
+++ TRIANGLE.java
@@ -0,0 +1,14 @@
+package aaaa;
+
+public class TRIANGLE {
+	public static void main(String[] args) {
+        double base = 8;
+        double height = 6;
+
+        double area = (base * height) / 2;
+
+        System.out.println("Area of Triangle = " + area);
+    }
+
+
+}
