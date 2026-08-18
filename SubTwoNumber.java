c22572cf6034c46bceb92985179d8a5324974273
ushakrishnegowda06 <ushakrishnegowda06@gmail.com>
2026-08-18 09:17:06 +0530

    Add files via upload

---
+++ SubTwoNumber.java
@@ -0,0 +1,15 @@
+package MY;
+
+public class SubTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		int a = 10;
+        int b = 5;
+        int c = a - b;
+
+        System.out.println("Subtraction = " + c);
+
+	}
+
+}
